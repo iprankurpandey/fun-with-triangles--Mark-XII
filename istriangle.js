e9b9c9fc2c8670c53fbdd8444aa6bcf4eecf0925
iprankurpandey <pprankur@gmail.com>
2021-08-28 13:05:50 +0530

    added triangle js file

---
+++ istriangle.js
@@ -0,0 +1,23 @@
+const inputs = document.querySelectorAll(".angle-input")
+const isTriangleBtn = document.querySelector("#is-triangle-btn")
+const outputE1 = document.querySelector("#output")
+
+function calculateSumOfAngles(angle1,angle2,angle3){
+    const sumOfAngles = angle1+angle2+angle3;
+    // console.log(sumOfAngles)
+    return sumOfAngles;
+}
+
+function isTriangle(){
+   const sumOfAngles = calculateSumOfAngles(Number(inputs[0].value),Number( inputs[1].value), Number(inputs[2].value))
+    if (sumOfAngles===180){
+        // console.log("yay ,the angle forms a triangle")
+        outputE1.innerText="yay ,the angle forms a triangle"
+    }
+    else{
+        // console.log("the angles dont form a triangle")
+        outputE1.innerText="the angles dont form a triangle"
+    }
+}
+
+isTriangleBtn.addEventListener("click", isTriangle)
